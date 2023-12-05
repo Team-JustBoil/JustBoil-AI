3b82e3bdbe3db5d19671542ed0831a2d7c2cbd3a
ddongseop <dlehdtjq00o@gmail.com>
2023-12-06 02:33:40 +0900

    [FEAT] 프롬프트 엔지니어링

--- main.py
+++ main.py
@@ -89,26 +89,35 @@ def get_recipe_recommendation():
 
     # OpenAI API를 사용하여 요리 추천
     prompt = """
-    현재 시간은 %s이고, 온도는 %s도, 날씨는 %s입니다. 상황에 맞는 음식을 추천해주세요. 중요도는 온도가 가장 높으며, 그 다음으로 날씨와 시간이 옵니다. 
-    추천된 음식과 그에 대한 코멘트를 JSON 형식으로 반환해주세요. 예를 들어, 추천 음식이 '된장찌개'라면, 반환 형식은 다음과 같습니다:
-
+    현재 시간은 %s이고, 온도는 %s도, 날씨는 %s입니다. 온도, 날씨, 시간에 맞게 요리해먹을 음식을 추천해주세요.
+    이때 음식 이름 외에 다른 형용사나 단어가 붙으면 안됩니다. 예를들어 "따뜻한 김치찌개"가 아닌 "김치찌개"를 추천해주세요. ("따뜻한"은 생략)
+    추천된 음식과 그에 대한 코멘트를 JSON 형식으로 반환해주세요. 반드시 이 형식을 지켜야 하며, "{" 와 "}" 도 빼먹지 말아주세요! // ``` 같은 주석 작성도 안됩니다.
+    예를 들어, 추천 음식이 '된장찌개'라면, 반환 형식은 다음과 같습니다:
+    
     {
-    "recommend": "된장찌개",
-    "comment": "오늘은 날씨가 추워 '된장찌개'를 추천합니다."
+        "recommend": "된장찌개",
+        "comment": "오늘은 날씨가 추워 '된장찌개'를 추천합니다. 집에서 따뜻한 된장찌개를 해먹어보시는 것은 어떨까요?"
     }
+
+    위의 모든 사항을 반드시 반영해서 음식을 추천해주세요. 저에게 매우 중요한 일입니다.
+    추가로 팁을 많이 드리겠습니다.
+
     """ % (current_time, current_temp, current_condition)
 
-    # ChatGPT에게 요청
-    completion = client.completions.create(
-        model="gpt-3.5-turbo-instruct",
-        prompt=prompt,
-        max_tokens=300,  # 음식 추천 텍스트의 최대 길이
-        temperature=0.7  # 응답의 다양성 조절
-    )
+    completion = client.chat.completions.create(
+            model="gpt-3.5-turbo-1106",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": prompt},
+            ],
+            max_tokens=300,  # 음식 추천 텍스트의 최대 길이
+            temperature=0.7  # 응답의 다양성 조절
+        )
+    print(completion.choices[0].message.content.strip())
 
     # 결과에서 JSON 형식의 음식 추천 가져오기
     try:
-        recipe_recommendation = json.loads(completion.choices[0].text.strip())
+        recipe_recommendation = json.loads(completion.choices[0].message.content.strip())
     except json.JSONDecodeError:
         # JSON 파싱 에러 처리
         recipe_recommendation = "JSON 파싱 에러 발생"
